54cb9f281f4fd06ceac62d7ddc9bdd12853a522d
Towser <scootscootaloo@outlook.com>
2024-07-31 23:28:16 +0000

    commit

--- app/src/main/java/com/example/testapp/secondactivity.kt
+++ app/src/main/java/com/example/testapp/secondactivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.example.testapp.databinding.ActivityMainBinding
+import com.example.testapp.databinding.ActivitySecondactivityBinding
+import com.google.android.material.navigation.NavigationView
 
 class Secondactivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -14,9 +18,21 @@ class Secondactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-       binding = ActivityMainBinding.inflate(layoutInflater)
+       binding = ActivitySecondactivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding
+        setSupportActionBar(binding.appBarMain.toolbar)
+        val drawLayout:DrawerLayout = binding.drawerLayout
+        val navView:NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home,R.id.nav_website, R.id.nav_about
+            )
+        )
+
+
+
+
 
         }
     }

ca1d635d84aa1ae137bfb092416ffc881d6cd7d3
Towser <scootscootaloo@outlook.com>
2024-08-01 00:05:01 +0000

    commit

--- app/src/main/java/com/example/testapp/secondactivity.kt
+++ app/src/main/java/com/example/testapp/secondactivity.kt
@@ -8,12 +8,14 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.testapp.databinding.ActivityMainBinding
 import com.example.testapp.databinding.ActivitySecondactivityBinding
 import com.google.android.material.navigation.NavigationView
 
 class Secondactivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding : ActivitySecondactivityBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,12 +29,17 @@ class Secondactivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.nav_home,R.id.nav_website, R.id.nav_about
-            )
+            ),drawLayout
         )
-
-
-
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        navView.setupWithNavController(navController)
 
 
         }
+
+    override fun onSupportNavigateUp(): Boolean {
+       val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
     }
